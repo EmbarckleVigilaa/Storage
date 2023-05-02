02bb19a891cb97e67083529b636047e12fe2da49
vigilaa <vigilaakennedy@gmail.com>
2023-05-02 13:46:40 +0530

    modified and added css

--- existing.js
+++ existing.js
@@ -1,55 +1,48 @@
 var inputDate = document.getElementById("date");
 var inputContent = document.getElementById("content");
 var submitButton = document.getElementById("submitBtn");
-var updateButton=document.getElementById("updatebtn");
 
 
-submitButton.addEventListener("click", () => {
-    var date = inputDate.value;
-    var content = inputContent.value;
-    var data = { 'date_' : date , 'content' : content };
-    var $myData=JSON.parse(localStorage.getItem("myData"));
-    let $ifAvaliable = false
+var $myData=JSON.parse(localStorage.getItem("myData"));
+inputDate.addEventListener("change", () => {
+    let date = inputDate.value;
+     if($myData){
+     for(let i=0;i<$myData.length;i++)
+      if($myData[i].date_===date){
+   var res=confirm("Data already exists, Do you wish to edit it?") 
+   if(res==true){
+  var existing=JSON.stringify($myData[i].content);
+    inputContent.value=existing;  
+ }else{
+  console.log("no need to edit")
+       }
+       }
+        }
+        localStorage.setItem("myData",JSON.stringify($myData));
+ });
+     submitButton.addEventListener("click",()=>{ 
+        let data = { 'date_' : inputDate.value , 'content' : inputContent.value };
+        let date = inputDate.value;
+        let content = inputContent.value;
     if(!$myData){
-        $myData=[];
-    }else{
-        for (let i = 0; i < $myData.length ; i++) {
-            if ($myData[i].date_ === date) {
-            $ifAvaliable = true
-            var existing=$myData[i];
+         $myData=[];
+    }
+    let $isavaliable = false
+        for(let i=0;i<$myData.length;i++){
+            if($myData[i].date_ === date){
+                $isavaliable = true
+            }
+        }   
+        if(!$isavaliable){
+            $myData.push(data)
         }
-    }
-    }
-    if ($ifAvaliable ) {
-    var res=confirm("Data already exists, Do you wish to edit it?") 
-    if(res==true){
-        
-      inputContent.value =  existing.content
-      updateButton.addEventListener("click", () => {
-      
-      for (let i = 0; i < $myData.length ; i++) {
-        if ($myData[i].date_ === date) {
-            var x = document.getElementById("content").value;
-      $myData[i].content=x;
-      break;
-        }
-    }
+        for(let i=0;i<$myData.length;i++){
+            if($myData[i].date_=== date){
+                $myData[i].content=inputContent.value;
+            }
+        }   
     localStorage.setItem("myData",JSON.stringify($myData));
-            });
 
-      localStorage.setItem()
-     localStorage.clear(existing)
-      localStorage.setItem("myData", JSON.stringify($myData));
-    }else{
-        console.log("no need to edit")
-    }
-    }     
-    else{
-        $myData.push(data)
-    }
-   
-    localStorage.setItem("myData", JSON.stringify($myData));
+    inputContent.value = ""
+    inputDate.value = ""
 });
-
-
-
